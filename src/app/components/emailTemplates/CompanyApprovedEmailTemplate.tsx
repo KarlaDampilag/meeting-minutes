9c02e0aa1c76798d17521f9cdeb188946492b12d
Karla Dampilag <karla.dmplg@gmail.com>
2024-08-04 13:15:22 +0200

    Minor email copy update

--- src/app/components/emailTemplates/CompanyApprovedEmailTemplate.tsx
+++ src/app/components/emailTemplates/CompanyApprovedEmailTemplate.tsx
@@ -17,7 +17,7 @@ export const CompanyApprovedEmailTemplate: React.FC<Readonly<EmailTemplateProps>
         <ul className='list-disc ml-6'>
             <li><Link href={`${process.env.NEXT_PUBLIC_APP_URL}/dashboard/team`}>Invite your team members and assign their roles</Link></li>
             <li><Link href={`${process.env.NEXT_PUBLIC_APP_URL}/dashboard/properties`}>Manage and update property details</Link></li>
-            <li><Link href={`${process.env.NEXT_PUBLIC_APP_URL}/dashboard/company-settings`}>Review and update your company settings</Link></li>
+            <li><Link href={`${process.env.NEXT_PUBLIC_APP_URL}/dashboard/company-settings`}>Review and update your company details</Link></li>
         </ul>
         <p>As a new user, your team can enjoy a 7-day free trial to explore all features.</p>
         <p>We&apos;re thrilled to have you on board and look forward to supporting your success!</p>
