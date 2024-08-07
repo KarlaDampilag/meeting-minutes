e7eee15c23ea14f3a3169e21c15f994476383458
Karla Dampilag <karla.dmplg@gmail.com>
2024-08-07 12:11:14 +0200

    Update accept invite email template

--- src/app/components/emailTemplates/UserInviteEmailTemplate.tsx
+++ src/app/components/emailTemplates/UserInviteEmailTemplate.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Button } from "@react-email/components";
+import { Link } from "@react-email/components";
 
 interface EmailTemplateProps {
     inviteId: string;
@@ -13,6 +13,6 @@ export const UserInviteEmailTemplate: React.FC<Readonly<EmailTemplateProps>> = (
     <div>
         <h1>You&apos;ve been invited to {process.env.NEXT_PUBLIC_APP_NAME}</h1>
         <p>You&apos;ve been invited to join as a member of {companyName}&apos;s account on {process.env.NEXT_PUBLIC_APP_NAME}.</p>
-        <p>To accept this invitation and start using {process.env.NEXT_PUBLIC_APP_NAME}, click on this link:</p> <Button href={`${process.env.NEXT_PUBLIC_APP_URL}/accept-invite?id=${inviteId}`} className='bg-primary px-4 py-3'>Sign up</Button>
+        <p>To accept this invitation and start using {process.env.NEXT_PUBLIC_APP_NAME}, click on this link:</p> <Link href={`${process.env.NEXT_PUBLIC_APP_URL}/accept-invite?id=${inviteId}`} className='bg-primary px-4 py-3'>Sign up</Link>
     </div>
 );
