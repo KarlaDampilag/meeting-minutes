316e0adf68f826d038727ec16a53f687e8d2809f
Karla Dampilag <karla.dmplg@gmail.com>
2024-08-07 12:14:05 +0200

    More email template updates

--- src/app/components/emailTemplates/UserInviteEmailTemplate.tsx
+++ src/app/components/emailTemplates/UserInviteEmailTemplate.tsx
@@ -13,6 +13,6 @@ export const UserInviteEmailTemplate: React.FC<Readonly<EmailTemplateProps>> = (
     <div>
         <h1>You&apos;ve been invited to {process.env.NEXT_PUBLIC_APP_NAME}</h1>
         <p>You&apos;ve been invited to join as a member of {companyName}&apos;s account on {process.env.NEXT_PUBLIC_APP_NAME}.</p>
-        <p>To accept this invitation and start using {process.env.NEXT_PUBLIC_APP_NAME}, click on this link:</p> <Link href={`${process.env.NEXT_PUBLIC_APP_URL}/accept-invite?id=${inviteId}`} className='bg-primary px-4 py-3'>Sign up</Link>
+        <p>To accept this invitation and start using {process.env.NEXT_PUBLIC_APP_NAME}, click on this link: <Link href={`${process.env.NEXT_PUBLIC_APP_URL}/accept-invite?id=${inviteId}`} className='bg-primary px-4 py-3'>Sign up</Link></p> 
     </div>
 );
